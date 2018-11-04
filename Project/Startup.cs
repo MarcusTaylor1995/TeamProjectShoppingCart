adfb1ffac1ec5fe77f807e68dc644a33041f402d
Gabriel Dos Santos <GMONTEIRODOSSA@augusta.edu>
2018-11-04 03:06:23 -0500

    Set start page

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -67,7 +67,7 @@ namespace Project
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=UserAccount}/{action=LoginView}/{id?}");
             });
         }
     }
